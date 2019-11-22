f38ebe22d7f4d6ecb914d8f817f50d4a92f1f049
Cezary Fąfara <ceziu1997@gmail.com>
2019-11-22 11:12:42 +0100

    Init commit

---
+++ src/main/java/com/photon/ChargeIO/ChargeIoApplication.java
@@ -0,0 +1,13 @@
+package com.photon.ChargeIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photon/ChargeIO/ChargeIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photon.ChargeIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
